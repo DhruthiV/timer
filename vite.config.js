eab447f384f818b5251a741eedcb1ac712572024
DhruthiV <dhruthivenkatesh.pmcs.369@gmail.com>
2025-08-06 21:58:02 +0530

    base url change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/timer/",
+  base: "/timer",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
